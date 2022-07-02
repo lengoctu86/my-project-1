be20a70d35c871be54db213eaaa3b83e8e1e84f0
lengoctu86 <lengoctu86@gmail.com>
2022-07-02 14:17:29 +0700

    First commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
+public class TestController {
+
+    @GetMapping
+    public String getText () {
+        return "tuln2";
+    }
+
+}
