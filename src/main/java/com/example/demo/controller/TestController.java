49380070dd313f7f7a626e0e11d69d6b98412b89
lengoctu86 <lengoctu86@gmail.com>
2022-07-08 07:13:31 +0700

    new commit

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/test")
 public class TestController {
 
+    private static final Logger log = LogManager.getLogger(TestController.class);
+
     @GetMapping
     public String getText () {
-        return "Nhung sing";
+        log.info("Start API!");
+        String output = "some output 1";
+        log.info("End API! Return " + output);
+        return output;
     }
 
 }
